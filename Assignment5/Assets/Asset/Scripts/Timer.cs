de017e372fbb8e6cd5757ebd00c2a4b0680a0736
Evita Lobo <elobo@ucsc.edu>
2019-03-04 15:32:22 -0800

    Maze layout

---
+++ Assignment5/Assets/Asset/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+
+    public float timeLeft = 60.0f;
+    public Text timer; 
+
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timer.text = (timeLeft).ToString("0");
+        if (timeLeft < 0)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
